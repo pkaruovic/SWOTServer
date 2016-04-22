1c4aa0ae9881ba991e6baa60712c61f0a2b484e7
pkaruovic <pkaruovic@gmail.com>
2016-04-22 15:34:28 +0200

    Napravljena klasa glavniProzor

---
+++ SWOT/src/gui/GlavniProzor.java
@@ -0,0 +1,128 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.GridLayout;
+import javax.swing.JTable;
+
+public class GlavniProzor extends JFrame {
+
+	private JPanel contentPane;
+	private JMenuBar menuBar;
+	private JMenu mnFile;
+	private JMenu mnHelp;
+	private JMenuItem mntmOpen;
+	private JMenuItem mntmSave;
+	private JPanel panel;
+	private JTable Snage;
+	private JTable Slabosti;
+	private JTable Sanse;
+	private JTable Pretnje;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GlavniProzor frame = new GlavniProzor();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GlavniProzor() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setJMenuBar(getMenuBar_1());
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(getPanel(), BorderLayout.CENTER);
+	}
+
+	private JMenuBar getMenuBar_1() {
+		if (menuBar == null) {
+			menuBar = new JMenuBar();
+			menuBar.add(getMnFile());
+			menuBar.add(getMnHelp());
+		}
+		return menuBar;
+	}
+	private JMenu getMnFile() {
+		if (mnFile == null) {
+			mnFile = new JMenu("File");
+			mnFile.add(getMntmOpen());
+			mnFile.add(getMntmSave());
+		}
+		return mnFile;
+	}
+	private JMenu getMnHelp() {
+		if (mnHelp == null) {
+			mnHelp = new JMenu("Help");
+		}
+		return mnHelp;
+	}
+	private JMenuItem getMntmOpen() {
+		if (mntmOpen == null) {
+			mntmOpen = new JMenuItem("Open");
+		}
+		return mntmOpen;
+	}
+	private JMenuItem getMntmSave() {
+		if (mntmSave == null) {
+			mntmSave = new JMenuItem("Save");
+		}
+		return mntmSave;
+	}
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setLayout(new GridLayout(1, 0, 0, 0));
+			panel.add(getSnage());
+			panel.add(getSlabosti());
+			panel.add(getSanse());
+			panel.add(getPretnje());
+		}
+		return panel;
+	}
+	private JTable getSnage() {
+		if (Snage == null) {
+			Snage = new JTable();
+		}
+		return Snage;
+	}
+	private JTable getSlabosti() {
+		if (Slabosti == null) {
+			Slabosti = new JTable();
+		}
+		return Slabosti;
+	}
+	private JTable getSanse() {
+		if (Sanse == null) {
+			Sanse = new JTable();
+		}
+		return Sanse;
+	}
+	private JTable getPretnje() {
+		if (Pretnje == null) {
+			Pretnje = new JTable();
+		}
+		return Pretnje;
+	}
+}
