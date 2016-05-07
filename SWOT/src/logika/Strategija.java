a5e01cc141604fb75b562b47046f93a109cc7332
andrijadjordjevic1995 <andrija.djordjevic1995@gmail.com>
2016-05-07 18:52:36 +0200

    Dokumentacija za klasu strategija

--- SWOT/src/logika/Strategija.java
+++ SWOT/src/logika/Strategija.java
@@ -4,6 +4,15 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author Andrija Djordjevic 
+ * 
+ * Klasa koja predstavlja strategiju koju korisnik kreira kombinovanjem snaga,
+ *  slabosti, sansi i pretnji i odgovarajucih atratktivnosti.
+ *  
+ *
+ */
 public class Strategija implements Serializable{
 	private String naziv;
 	private double sumaUkupnihAtraktivnosti;
@@ -17,6 +26,13 @@ public class Strategija implements Serializable{
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Parametrizovani konstruktor koji pri kreiranju strategije kreira i prazne liste snaga,
+	 *  slabosti, sansi i pretnji u koje ce biti unete iste. Suma ukupnih atraktivnosti se inicijalizuje 
+	 *  na 0, a kada korisnik zavrsi sa unosom snaga, slabosti, sansi i pretnji i odgovarajucih
+	 *  atraktivnosti, suma ukpnih atraktivnoti se izracunava.
+	 * @param naziv 
+	 */
 	public Strategija(String naziv) {
 		this.naziv = naziv;
 		snage = new ArrayList<SwotStrat>();
@@ -26,53 +42,94 @@ public class Strategija implements Serializable{
 		sumaUkupnihAtraktivnosti = 0;
 	}
 	
-	
+	/**
+	 * 
+	 * @return naziv strategije
+	 */
 	public String getNaziv() {
 		return naziv;
 	}
 
-
+	/**
+	 * 
+	 * @return double
+	 */
 	public double getSumaUkupnihAtraktivnosti() {
 		return sumaUkupnihAtraktivnosti;
 	}
 
-
+	/**
+	 * 
+	 * @return lista snaga dodatih u ovu strategiju
+	 */
 	public List<SwotStrat> getSnage() {
 		return snage;
 	}
 
-
+	/**
+	 * 
+	 * @return lista slabosti dodatih u ovu strategiju
+	 */
 	public List<SwotStrat> getSlabosti() {
 		return slabosti;
 	}
 
-
+	/**
+	 * 
+	 * @return lista sansi dodatih u ovu strategiju
+	 */
 	public List<SwotStrat> getSanse() {
 		return sanse;
 	}
 
-
+	/**
+	 * 
+	 * @return lista pretnji dodatih u ovu strategiju
+	 */
 	public List<SwotStrat> getPretnje() {
 		return pretnje;
 	}
 
-//ukloni visak, moze samo swotstrat da se prosledjuje
+	/**
+	 * Metoda dodaje snagu u listu snaga ove strategije
+	 * @param snaga
+	 * @param atraktivnost
+	 */
 	public void dodajSnagu(SwotStrat snaga, int atraktivnost){
 		SwotStrat s = new SwotStrat(snaga.getNaziv(), snaga.getPonder(), atraktivnost);
 		snage.add(s);
 	}
+	/**
+	 * Metoda dodaje slabost u listu slabosti ove strategije
+	 * @param slabost
+	 * @param atraktivnost
+	 */
 	public void dodajSlabost(SwotStrat slabost, int atraktivnost){
 		SwotStrat s = new SwotStrat(slabost.getNaziv(), slabost.getPonder(), atraktivnost);
 		slabosti.add(s);
 	}
+	/**
+	 * Metoda dodaje sansu u listu sansi ove strategije
+	 * @param sansa
+	 * @param atraktivnost
+	 */
 	public void dodajSansu(SwotStrat sansa, int atraktivnost){
 		SwotStrat s = new SwotStrat(sansa.getNaziv(), sansa.getPonder(), atraktivnost);
 		sanse.add(s);
 	}
+	/**
+	 * Metoda dodaje pretnju u listu pretnji ove strategije
+	 * @param pretnja
+	 * @param atraktivnost
+	 */
 	public void dodajPretnju(SwotStrat pretnja, int atraktivnost){
 		SwotStrat s = new SwotStrat(pretnja.getNaziv(), pretnja.getPonder(), atraktivnost);
 		pretnje.add(s);
 	}
+	/**
+	 * Metoda prolazi kroz liste snaga, slabosti, sansi, pretnji i sumira atraktivnosti. Suma ukupnih 
+	 * atraktivnosti predstavlja atraktivnost same strategije.
+	 */
 	public void izracunajSumuUkupnihAtraktivnosti(){
 		for (SwotStrat swotStrat : pretnje) {
 			sumaUkupnihAtraktivnosti+= swotStrat.getUkupnaAtraktivnost();
@@ -87,7 +144,10 @@ public class Strategija implements Serializable{
 			sumaUkupnihAtraktivnosti+= swotStrat.getUkupnaAtraktivnost();
 		}
 	}
-	
+	/**
+	 * Redefinisana equals metoda koja poredi objekte klase strategija na osnovu naziva.
+	 * @return true ako su nazivi strategija isti, false inace
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if(obj != null){
